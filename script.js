bd0e73535629a280f63ca4f32168c94595348bb8
jobaidulit <jobaidulit@gmail.com>
2026-03-07 22:08:01 +0600

    Choose trees section ok

--- script.js
+++ script.js
@@ -1,5 +1,27 @@
-
 const categoriesContainer = document.getElementById("categoriesContainer");
+const treesContainer = document.getElementById("treesContainer");
+const loadingSpinner = document.getElementById("loadingSpinner");
+const allTreesbtn = document.getElementById("allTreesbtn");
+const treeDetailsModal = document.getElementById("tree-details-modal");
+const modalImage = document.getElementById("modalImage");
+const modalCategory = document.getElementById("modalCategory");
+const modalDescription = document.getElementById("modalDescription");
+const modalPrice = document.getElementById("modalPrice");
+const modalTitle = document.getElementById("modalTitle");
+const cartContainer = document.getElementById("cartContainer");
+const totalPrice = document.getElementById("totalPrice");
+const emptyCartMessage = document.getElementById("emptyCartMessage");
+let cart = [];
+
+// Loading
+function showLoading() {
+  loadingSpinner.classList.remove("hidden");
+  treesContainer.innerHTML = "";
+}
+function hideLoading() {
+  loadingSpinner.classList.add("hidden");
+}
+
 // Category btn
 async function loadCategories() {
   // async await
@@ -19,15 +41,61 @@ async function loadCategories() {
   });
 }
 
-async function loadTrees() {
-  const res = await fetch("https://openapi.programming-hero.com/api/plants");
+async function selectCategory(categoryId, btn) {
+  console.log(categoryId, btn);
+  showLoading();
+
+  // Update active button style
+  const allButtons = document.querySelectorAll(
+    "#categoriesContainer button, #allTreesbtn",
+  );
+  //   console.log(allButtons);
+  allButtons.forEach((btn) => {
+    btn.classList.remove("btn-primary");
+    btn.classList.add("btn-outline");
+  });
+
+  btn.classList.add("btn-primary");
+  btn.classList.remove("btn-outline");
+
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/category/${categoryId}`,
+  );
   const data = await res.json();
+  console.log(data);
   displayTrees(data.plants);
+
+  hideLoading();
 }
 
-function displayTrees(trees){
- trees.forEach((tree)=>{
-     const card = document.createElement("div");
+allTreesbtn.addEventListener("click", () => {
+  // Update active button style
+  const allButtons = document.querySelectorAll(
+    "#categoriesContainer button, #allTreesbtn",
+  );
+  //   console.log(allButtons);
+  allButtons.forEach((btn) => {
+    btn.classList.remove("btn-primary");
+    btn.classList.add("btn-outline");
+  });
+
+  allTreesbtn.classList.add("btn-primary");
+  allTreesbtn.classList.remove("btn-outline");
+
+  loadTrees();
+});
+
+// Trees load and display
+async function loadTrees() {
+  showLoading();
+  const res = await fetch("https://openapi.programming-hero.com/api/plants");
+  const data = await res.json();
+  hideLoading();
+  displayTrees(data.plants);
+}
+function displayTrees(trees) {
+  trees.forEach((tree) => {
+    const card = document.createElement("div");
     card.className = `card bg-white shadow-sm border-b-2 ${tree.price > 500 ? "border-red-500" : "border-green-500"}`;
     card.innerHTML = `<figure>
         <img
@@ -51,9 +119,74 @@ function displayTrees(trees){
         </div>
       </div>`;
     treesContainer.appendChild(card);
-  })
+  });
 }
+async function openTreeModal(treeId) {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/plant/${treeId}`,
+  );
+  const data = await res.json();
+  const plantDetails = data.plants;
+  modalTitle.textContent = plantDetails.name;
+  modalImage.src = plantDetails.image;
+  modalCategory.textContent = plantDetails.category;
+  modalDescription.textContent = plantDetails.description;
+  modalPrice.textContent = plantDetails.price;
+  treeDetailsModal.showModal();
+}
+loadCategories();
+loadTrees();
 
-loadCategories()
-loadTrees()
+// cart
+function addToCart(id, name, price) {
+  console.log(id, name, price, "add to cart");
+  const existingItem = cart.find((item) => item.id === id);
 
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      id,
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+
+  updateCart();
+}
+function updateCart() {
+  cartContainer.innerHTML = "";
+
+  if (cart.length === 0) {
+    emptyCartMessage.classList.remove("hidden");
+    totalPrice.textContent = `$${0}`;
+    return;
+  }
+
+  emptyCartMessage.classList.add("hidden");
+
+  let total = 0;
+  cart.forEach((item) => {
+    total += item.price * item.quantity;
+    const cartItem = document.createElement("div");
+    cartItem.className = "card card-body bg-slate-100 font-semibold";
+    cartItem.innerHTML = `<div class="flex justify-between items-center">
+                                <div>
+                                    <h2>${item.name}</h2>
+                                    <p> $${item.price} × ${item.quantity}</p>
+                                </div>
+                                <button class="btn btn-ghost" onclick="removeFromCart(${item.id})">X</button>
+                            </div>
+                            <p class="text-right font-semibold text-xl">$${item.price * item.quantity}</p>`;
+
+    cartContainer.appendChild(cartItem);
+  });
+
+  totalPrice.innerText = `$${total}`;
+}
+function removeFromCart(treeId) {
+  const updatedCartElements = cart.filter((item) => item.id != treeId);
+  cart = updatedCartElements;
+  updateCart();
+}
